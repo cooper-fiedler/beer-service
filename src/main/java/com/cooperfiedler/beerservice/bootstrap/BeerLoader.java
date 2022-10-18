5278f40829ed5da23f85131ba936b340d2f41bbb
cooper-fiedler <103666397+cooper-fiedler@users.noreply.github.com>
2022-10-18 08:52:27 -0700

    Added BeerLoader

---
+++ src/main/java/com/cooperfiedler/beerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,47 @@
+package com.cooperfiedler.beerservice.bootstrap;
+
+import com.cooperfiedler.beerservice.domain.Beer;
+import com.cooperfiedler.beerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() ==0){
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+
+        }
+
+    }
+}
